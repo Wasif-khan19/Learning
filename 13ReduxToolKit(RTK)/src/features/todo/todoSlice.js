7b5b1155d281e6467eddb9caa45616013bf51185
Wasif <wk5599426@gmail.com>
2024-08-23 17:21:16 +0500

    editred

--- 13ReduxToolKit(RTK)/src/features/todo/todoSlice.js
+++ 13ReduxToolKit(RTK)/src/features/todo/todoSlice.js
@@ -1,14 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos:[{id:1, text:"Hello"}]
-}
+  todos: [{ id: 1, text: "Hello worl" }],
+};
 
 export const todoSlice = createSlice({
-    name: 'todo',
-    initialState,
-    reducers: {
-        addTodo: ()=>{},
-        removeTodo:()=>{}
-    }
-})
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo)
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo)=>todo.id !== action.payload)
+    },
+  },
+});
+ 
