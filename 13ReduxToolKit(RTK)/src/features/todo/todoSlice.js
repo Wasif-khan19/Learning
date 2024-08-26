d17bb928ff57d9eed64beaa12fc0bc94fe92d830
Wasif <wk5599426@gmail.com>
2024-08-26 13:16:24 +0500

    edited

--- 13ReduxToolKit(RTK)/src/features/todo/todoSlice.js
+++ 13ReduxToolKit(RTK)/src/features/todo/todoSlice.js
@@ -13,11 +13,10 @@ export const todoSlice = createSlice({
         id: nanoid(),
         text: action.payload,
       };
-      state.todos.push(todo)
+      state.todos.push(todo);
     },
     removeTodo: (state, action) => {
-      state.todos = state.todos.filter((todo)=>todo.id !== action.payload)
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
   },
 });
- 
